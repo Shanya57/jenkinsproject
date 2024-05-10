a3ded30c3cf67502213e9c713ab39ebd14b13ce5
Shanya57 <83780333+Shanya57@users.noreply.github.com>
2024-05-10 16:09:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# This is a simple flask app that displays "Hello World"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+    return "<h2>Hello world - Integration Activity Flask Deployment Lab: try 2 <h2><hr/>"\
+
+
+app.run(host="0.0.0.0", port=5000)
